7d3081a370daa655f95ea3c9b8b69671c8bfb512
kainino0x <kainino1@gmail.com>
2026-01-03 06:16:22 +0000

    Deploying to gh-pages from @ kainino0x/cts@553ce8940a3aa1ade9f75e8e78462f4504f22b62 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '2956fd88f644fe89774fc1693e0fd21cace0be39';
+export const version = '553ce8940a3aa1ade9f75e8e78462f4504f22b62';
